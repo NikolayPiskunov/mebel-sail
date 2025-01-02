6799ef58eaf6604fc7bc0ff39970f30f1965e09f
Nikolay Piskunov <nikolayrostow@mail.ru>
2025-01-02 11:34:46 +0300

    009: добавил seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,14 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'nick@mail.com',
+         ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::factory(10)->create();
 
-//        Order::factory(100)->create();
-//        Client::factory(25)->create();
+        Order::factory(100)->create();
+        Client::factory(25)->create();
     }
 }
